15c9c7beb5e604f2d9ecbd6dfcff61203788a581
Sanan Mammadov <sananmammadov99@gmail.com>
2022-07-31 22:11:15 +0400

    update method

--- yummy/main.py
+++ yummy/main.py
@@ -63,6 +63,7 @@ def delete_menu(menu_id):
     flash('Menu item was successfully deleted')
     return redirect(url_for('menu'))
 
+
 # create menu item
 @app.route('/menu/<int:menu_id>/menu_item/new', methods=['GET', 'POST'])
 def new_menu_item(menu_id):
@@ -102,8 +103,49 @@ def delete_menu_item(menu_id, menu_item_id):
     db.session.commit()
     flash('Menu item was successfully deleted')
     return redirect(url_for('menu_item', menu_id=menu_id))
+
+
+# edit menu
+@app.route('/menu/<int:menu_id>/edit', methods=['GET', 'POST'])
+def edit_menu(menu_id):
+    menu = Menu.query.get_or_404(menu_id)
+    if request.method == 'POST':
+        menu.name = request.form['name']
+        db.session.commit()
+        return redirect(url_for('menu'))
+    return render_template('edit_menu.html', menu=menu)
     
-# get all
+
+
+# update menu item
+@app.route('/menu/<int:menu_id>/menu_item/<int:menu_item_id>/edit', methods=['GET', 'POST'])
+def edit_menu_item(menu_id, menu_item_id):
+    menu_item = MenuItem.query.get_or_404(menu_item_id)
+    if request.method == 'POST':
+        menu_item.name = request.form['name']
+        menu_item.price = request.form['price']
+        menu_item.ingredients = request.form['ingredients']
+        image = request.files['image']
+        if image.filename != '':
+            image_url = str(uuid.uuid4()) + '.' + image.filename.split('.')[-1]
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_url))
+            menu_item.image = image_url
+        db.session.commit()
+        return redirect(url_for('menu_item', menu_id=menu_id))
+    return render_template('edit_menu_item.html', menu_item=menu_item)\
+
+# edit menu
+@app.route('/menu/<int:menu_id>/edit', methods=['GET', 'POST'])
+def edit_menu(menu_id):
+    menu = Menu.query.get_or_404(menu_id)
+    if request.method == 'POST':
+        menu.name = request.form['name']
+        db.session.commit()
+        return redirect(url_for('menu'))
+    return render_template('edit_menu.html', menu=menu)
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
